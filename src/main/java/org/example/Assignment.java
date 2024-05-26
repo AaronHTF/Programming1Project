b4fdfd9c4f1ac3406e2edb072f7787687ff99f00
Aaron Ho <ajhotimfat@gmail.com>
2024-05-26 15:10:04 -0400

    Added calcAssignmentAvg, generateRandomScore and toString in Assignment class

--- src/main/java/org/example/Assignment.java
+++ src/main/java/org/example/Assignment.java
@@ -11,5 +11,25 @@ public class Assignment {
     private ArrayList<Integer> scores;
     private static int nextId = 1;
 
+    public void calcAssignmentAvg() {
+        int sum = 0;
+
+        for (int score : scores) {
+            sum += score;
+        }
+
+        assignmentAverage = (double) sum / scores.size();
+    }
+
+    public void generateRandomScore() {
+        //TODO finish method body
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Assignment ID: %s, Assignment Name: %s, Weight: %f, maxScore: %d",
+                assignmentId, assignmentName, weight, maxScore);
+    }
+
     //TODO add the rest of the methods
 }
