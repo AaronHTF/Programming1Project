cb33fdc7401b6b5f7a6d1e91e82fa9a65e4bc12f
Aaron Ho <ajhotimfat@gmail.com>
2024-05-25 20:05:17 -0400

    Created Address class, added fields, constructors, toString, equals, getter, setter and isPostalCodeValid method

---
+++ src/main/java/org/example/Address.java
@@ -0,0 +1,43 @@
+package org.example;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@ToString
+@EqualsAndHashCode
+@Getter
+@Setter
+public class Address {
+    private int streetNo;
+    private String street;
+    private String city;
+    private String province;
+    private String postalCode;
+    private String country;
+
+    public Address(int streetNo, String street, String city, String province, String postalCode, String country) {
+        if (isPostalCodeValid(postalCode)) {
+            this.streetNo = streetNo;
+            this.street = street;
+            this.city = city;
+            this.province = province;
+            this.postalCode = postalCode;
+            //TODO set postal code characters to uppercase
+            this.country = country;
+        } else {
+            this.streetNo = 0;
+            this.street = null;
+            this.city = null;
+            this.province = null;
+            this.postalCode = null;
+            this.country = null;
+        }
+    }
+
+    public static boolean isPostalCodeValid(String postalCode) {
+        return true;
+        //TODO complete this method body
+    }
+}
