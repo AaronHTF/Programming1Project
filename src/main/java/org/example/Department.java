51a08266624df8d26ff5aab552e6b54e45723762
Aaron Ho <ajhotimfat@gmail.com>
2024-05-25 20:49:08 -0400

    Created Department class, added fields, constructor and validateDepartmentName method

---
+++ src/main/java/org/example/Department.java
@@ -0,0 +1,19 @@
+package org.example;
+
+import lombok.NonNull;
+
+public class Department {
+    private String departmentId;
+    private String departmentName;
+    private static int nextId = 1;
+
+    public Department(@NonNull String departmentName) {
+        this.departmentId = String.format("D%02d", nextId++);
+        this.departmentName = departmentName;
+    }
+
+    public boolean validateDepartmentName(String departmentName) {
+        return true;
+        //TODO complete this method body
+    }
+}
